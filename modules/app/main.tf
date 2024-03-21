2d007d6607d352752f2ac76d5d2c6347b11a278a
dspriyasg <priya9.sdr@gmail.com>
2024-03-21 16:22:27 +0800

    project setupv1

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  ami                    = "data.aws_ami.image.id"
+  ami                    = "data.aws_ami.ami.image.id"
   instance_type          = "var.instance_type"
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
