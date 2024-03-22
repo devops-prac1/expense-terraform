9ee46e6f0b84b6731b3291c9e5f03efec6b0b099
dspriyasg <priya9.sdr@gmail.com>
2024-03-22 10:45:00 +0800

    project setupv1

--- main.tf
+++ main.tf
@@ -8,13 +8,23 @@ module "frontend" {
   zone_id = var.zone_id
 
 }
+module "backend" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component     = "backend"
+  ssh_user = var.ssh_user
+  ssh_pass= var.ssh_pass
+  env = var.env
+  zone_id = var.zone_id
 
-#module "backend" {
+}
+module "mysql" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component     = "mysql"
+  ssh_user = var.ssh_user
+  ssh_pass= var.ssh_pass
+  env = var.env
+  zone_id = var.zone_id
 
-
- # source = "./modules/app"
-#}
-
-#module "mysql" {
-  #source = "./modules/app"
-#}
+}
