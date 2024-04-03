1a07063a303bef7f3e7552f1930a65c03320edd2
dspriyasg <priya9.sdr@gmail.com>
2024-04-03 15:12:11 +0800

    ansible

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -19,7 +19,7 @@ resource "null_resource" "ansible" {
       password = var.ssh_pass
       host     = aws_instance.instance.public_ip
     }
-#  helping to commit
+
     inline = [
       "sudo pip3.11 install ansible",
       "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  expense.yml -e env=${var.env}  -e  role_name=${var.component}"
