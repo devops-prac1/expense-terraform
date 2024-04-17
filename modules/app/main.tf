d309fb0910430f9af1cfe278d058ae7c27425138
dspriyasg <priya9.sdr@gmail.com>
2024-04-17 16:20:24 +0800

    ignore AMI changes to re-create the instances

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -35,24 +35,24 @@ resource "aws_security_group" "main" {
   }
 }
 
-#resource "null_resource" "ansible" {
-#  provisioner "remote-exec" {
-#
-#    connection {
-#      type     = "ssh"
-#      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
-#      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
-#      host     = aws_instance.instance.public_ip
-#    }
-#
-#    inline = [
-#      "sudo pip3.11 install ansible hvac",
-#      "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}" ,
-#      "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json" ,
-#      "rm -f ~/secrets.json ~/app.json"
-#    ]
-#  }
-#}
+resource "null_resource" "ansible" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "sudo pip3.11 install ansible hvac",
+      "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}" ,
+      "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json" ,
+      "rm -f ~/secrets.json ~/app.json"
+    ]
+  }
+}
 
 resource "aws_route53_record" "record" {
   name    = "${var.component}-${var.env}"
