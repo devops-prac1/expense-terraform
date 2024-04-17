3c10ab6a3b667e3774412c9684189d1acfc84097
dspriyasg <priya9.sdr@gmail.com>
2024-04-17 15:51:15 +0800

    adding NAtGW and routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-
 resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
@@ -17,8 +16,6 @@ resource "aws_vpc_peering_connection" "main" {
   }
 }
 
-
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -26,6 +23,57 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.env}-igw"
   }
 }
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  route {
+    cidr_block                = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt-${count.index + 1}"
+  }
+}
+resource "aws_eip" "ngw" {
+  count  = length(var.public_subnets)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count  = length(var.public_subnets)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.env}-ngw-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
+
+
 
 resource "aws_subnet" "frontend" {
   count             = length(var.frontend_subnets)
@@ -45,6 +93,10 @@ resource "aws_route_table" "frontend" {
   route {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  route {
+    cidr_block                = "0.0.0.0/0"
+    nat_gateway_id            = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
@@ -75,6 +127,10 @@ resource "aws_route_table" "backend" {
   route {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  route {
+    cidr_block                = "0.0.0.0/0"
+    nat_gateway_id            = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
@@ -107,6 +163,10 @@ resource "aws_route_table" "db" {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
+  route {
+    cidr_block                = "0.0.0.0/0"
+    nat_gateway_id            = aws_nat_gateway.ngw[count.index].id
+  }
 
   tags = {
     Name = "${var.env}-db-rt-${count.index + 1}"
@@ -118,37 +178,6 @@ resource "aws_route_table_association" "db" {
   subnet_id      = aws_subnet.db[count.index].id
   route_table_id = aws_route_table.db[count.index].id
 }
-
-resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = {
-    Name = "${var.env}-public-subnet-${count.index + 1}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  count  = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block                = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  }
-  tags = {
-    Name = "${var.env}-public-rt-${count.index + 1}"
-  }
-}
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnets)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
-}
-
-
 
 resource "aws_route" "default-vpc" {
   route_table_id            = var.default_route_table_id
