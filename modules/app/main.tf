6a69f5f981b009b9414b5f5bdedd9d397fbbe435
dspriyasg <priya9.sdr@gmail.com>
2024-04-18 14:45:31 +0800

    adding load balancers

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -36,19 +36,24 @@ resource "aws_security_group" "main" {
 }
 
 resource "null_resource" "ansible" {
+  connection {
+    type     = "ssh"
+    user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+    password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+    host     = aws_instance.instance.private_ip
+  }
+
   provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
-      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
-      host     = aws_instance.instance.private_ip
-    }
-
     inline = [
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}" ,
       "ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible  expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json" ,
+      "rm -f ~/secrets.json ~/app.json"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
       "rm -f ~/secrets.json ~/app.json"
     ]
   }
@@ -60,5 +65,17 @@ resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl     = 30
+}
+resource "aws_lb" "main" {
+  count              = var.lb_needed ? 1 : 0
+  name               = "${var.env}-${var.component}-alb"
+  internal           = var.lb_type == "public" ? false : true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.lb_subnets
+
+  tags = {
+    Environment = "${var.env}-${var.component}-alb"
+  }
 }
 #comment
