39d0766c65d363c4373117479c609b032e7e6d05
dspriyasg <priya9.sdr@gmail.com>
2024-04-25 15:56:41 +0800

    adding user data to autoscaling group

---
+++ modules/app-asg/user-data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/devops-prac1/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
